edce439db796d48ac4862d7c39e263cd824821c1
daniilbataev <ragoga48@gmail.com>
2021-09-14 14:24:58 +0300

    remove console.log

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,6 @@ const required = (value: string) => (value === '' ? 'Заполните поле
 const Footer = () => {
   const form = useForm()
   const { submitting } = useFormState({ subscription: { submitting: true } })
-  console.log('render', submitting)
 
   return (
     <div>
